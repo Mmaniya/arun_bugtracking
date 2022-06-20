5b78df07e15b15a023026846110e0b6dbf8acb66
Mmaniya <mmaniya01@gmail.com>
2022-06-20 14:37:05 +0530

    second

--- qrtraindata.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Zxing\QrReader;
-require __DIR__ . "/vendor/autoload.php";
-$qrcode = new QrReader('../QRcode/'. $filename);
-$isbn = $qrcode->text();
-
-// https://www.the-qrcode-generator.com/
